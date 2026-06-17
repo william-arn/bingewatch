450f92f3986bd3568e004ac35bd97a68de523227
william-arn <willarn95@gmail.com>
2026-06-17 14:42:47 -0500

    added movie

---
+++ catalog-service/src/main/java/com/bingewatch/catalog/domain/Movie.java
@@ -0,0 +1,33 @@
+package com.bingewatch.catalog.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="movies")
+public class Movie{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String genre;
+    @Column(name = "release_year")
+    private Integer releaseYear;
+
+    protected Movie(){}
+
+    public Movie(String title, String genre, Integer releaseYear) {
+        this.title = title;
+        this.genre = genre;
+        this.releaseYear = releaseYear;
+    }
+
+    public Long getId(){return id;}
+    public String getTitle(){return title;}
+    public void setTitle(String title){this.title = title;}
+    public String getGenre(){return genre;}
+    public void setGenre(String genre){this.genre = genre;}
+    public Integer getYear() { return releaseYear; }
+    public void setYear(Integer releaseYear) { this.releaseYear = releaseYear; }
+
+}
