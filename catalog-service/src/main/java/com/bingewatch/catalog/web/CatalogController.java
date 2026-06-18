2d59a68cbc8d4921b521a3b37898c8e7d00f8f3e
william-arn <willarn95@gmail.com>
2026-06-17 20:48:56 -0500

    fixed formatting

--- catalog-service/src/main/java/com/bingewatch/catalog/web/CatalogController.java
+++ catalog-service/src/main/java/com/bingewatch/catalog/web/CatalogController.java
@@ -2,8 +2,8 @@ package com.bingewatch.catalog.web;
 
 import com.bingewatch.catalog.domain.Movie;
 import com.bingewatch.catalog.service.CatalogService;
-import com.bingewatch.catalog.web.dto.MovieResponse;
 import com.bingewatch.catalog.web.dto.MovieRequest;
+import com.bingewatch.catalog.web.dto.MovieResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
